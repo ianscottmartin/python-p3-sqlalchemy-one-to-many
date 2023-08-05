ea3948c14fb7e81fded79df3fe0f73a4d43dad83
Ian Scott Martin <ianscottmartin@gmail.com>
2023-08-04 21:11:29 -0600

    saywhat

--- lib/seed.py
+++ lib/seed.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 
 from models import Game, Review
 
-if __name__ == "__main__":
-    engine = create_engine("sqlite:///one_to_many.db")
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///one_to_many.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -18,30 +18,11 @@ if __name__ == "__main__":
 
     fake = Faker()
 
-    genres = [
-        "action",
-        "adventure",
-        "strategy",
-        "puzzle",
-        "first-person shooter",
-        "racing",
-    ]
-    platforms = [
-        "nintendo 64",
-        "gamecube",
-        "wii",
-        "wii u",
-        "switch",
-        "playstation",
-        "playstation 2",
-        "playstation 3",
-        "playstation 4",
-        "playstation 5",
-        "xbox",
-        "xbox 360",
-        "xbox one",
-        "pc",
-    ]
+    genres = ['action', 'adventure', 'strategy',
+        'puzzle', 'first-person shooter', 'racing']
+    platforms = ['nintendo 64', 'gamecube', 'wii', 'wii u', 'switch',
+        'playstation', 'playstation 2', 'playstation 3', 'playstation 4',
+        'playstation 5', 'xbox', 'xbox 360', 'xbox one', 'pc']
 
     games = []
     for i in range(50):
@@ -49,7 +30,7 @@ if __name__ == "__main__":
             title=fake.unique.name(),
             genre=random.choice(genres),
             platform=random.choice(platforms),
-            price=random.randint(5, 60),
+            price=random.randint(5, 60)
         )
 
         # add and commit individually to get IDs back
@@ -60,13 +41,16 @@ if __name__ == "__main__":
 
     reviews = []
     for game in games:
-        for i in range(random.randint(1, 5)):
+        for i in range(random.randint(1,5)):
             review = Review(
-                score=random.randint(0, 10), comment=fake.sentence(), game_id=game.id
+                score=random.randint(0, 10),
+                comment=fake.sentence(),
+                game_id=game.id
             )
 
             reviews.append(review)
-
+    
     session.bulk_save_objects(reviews)
     session.commit()
     session.close()
+    
